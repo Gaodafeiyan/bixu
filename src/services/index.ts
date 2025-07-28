878b62096008426d05db0c6ce7a92c2c864e45e2
Gaodafeiyan <candicexfx18@gmail.com>
2025-07-29 07:24:15 +0800

    将区块链服务移动到recharge-channel模块内，修复服务注册问题

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,5 @@
 // 服务注册文件
 export default {
-  'api::recharge-channel.blockchain-service': () => import('./blockchain-service'),
   'api::investment-service.investment-service': () => import('./investment-service'),
   'api::lottery-service.lottery-service': () => import('./lottery-service'),
   'api::log-service.log-service': () => import('./log-service'),
