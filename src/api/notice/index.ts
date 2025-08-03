7d26417585931d73bcb9cdee2bf843fe122083b4
Gaodafeiyan <candicexfx18@gmail.com>
2025-08-03 12:20:44 +0800

    fix: 删除notice API的index文件，Strapi不需要index文件

--- src/api/notice/index.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  routes: () => import('./routes/notice'),
-  controllers: () => import('./controllers/notice'),
-  contentTypes: () => import('./content-types/notice/schema'),
-}; 
