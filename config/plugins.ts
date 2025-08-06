4b4ee726c67c817d6ed2505f782c11f2b1868dc4
Gaodafeiyan <candicexfx18@gmail.com>
2025-08-06 20:41:52 +0800

    禁用Strapi默认注册路由 - 解决/api/auth/register 405错误问题

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,6 +6,10 @@ export default ({ env }) => ({
       },
       register: {
         defaultRole: 'authenticated'
+      },
+      // 禁用默认的注册路由，使用我们自定义的路由
+      routes: {
+        register: false
       }
     }
   },
